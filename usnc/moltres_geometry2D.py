73d8c23aa13afbea4abe85bc9830c95947b2b9e7
robfairh <ref3@illinois.edu>
2019-10-18 18:04:17 -0500

    moltres geo2D bug?

--- usnc/moltres_geometry2D.py
+++ usnc/moltres_geometry2D.py
@@ -6,7 +6,7 @@ import numpy as np
 import math as mt
 import random as rd
 
-def add_lines(f, d_x, x):
+def place_hexagon(f, d_x, x, c, l, ns):
     """
     Adds boundary lines
     returns:
@@ -17,30 +17,28 @@ def add_lines(f, d_x, x):
     d = d_x/2 * np.tan(np.pi/3)
     d = round(d, 4)
 
-    f.write("//+\n")
-    f.write("SetFactory('OpenCASCADE');\n")
     f.write("// Define Points of the assembly boundaries \n")
-    f.write("Point(1) = { "+ str(x[0] + d_x/2) +", "+ str(x[1] + d) +", "+ str(x[2]) +", 1.0};\n")
-    f.write("Point(2) = { "+ str(x[0] + d_x) +", "+ str(x[1]) +", "+ str(x[2]) +", 1.0};\n")
-    f.write("Point(3) = { "+ str(x[0] + d_x/2) +", "+ str(x[1] - d) +", "+ str(x[2]) +", 1.0};\n")
-    f.write("Point(4) = { "+ str(x[0] - d_x/2) +", "+ str(x[1] - d) +", "+ str(x[2]) +", 1.0};\n")
-    f.write("Point(5) = { "+ str(x[0] - d_x) +", "+ str(x[1]) +", "+ str(x[2]) +", 1.0};\n")
-    f.write("Point(6) = { "+ str(x[0] - d_x/2) +", "+ str(x[1] + d) +", "+ str(x[2]) +", 1.0};\n")
+    f.write("Point("+ str(c) +") = { "+ str(x[0] + d_x/2) +", "+ str(x[1] + d) +", "+ str(x[2]) +", 1.0};\n")
+    f.write("Point("+ str(c + 1) +") = { "+ str(x[0] + d_x) +", "+ str(x[1]) +", "+ str(x[2]) +", 1.0};\n")
+    f.write("Point("+ str(c + 2) +") = { "+ str(x[0] + d_x/2) +", "+ str(x[1] - d) +", "+ str(x[2]) +", 1.0};\n")
+    f.write("Point("+ str(c + 3) +") = { "+ str(x[0] - d_x/2) +", "+ str(x[1] - d) +", "+ str(x[2]) +", 1.0};\n")
+    f.write("Point("+ str(c + 4) +") = { "+ str(x[0] - d_x) +", "+ str(x[1]) +", "+ str(x[2]) +", 1.0};\n")
+    f.write("Point("+ str(c + 5) +") = { "+ str(x[0] - d_x/2) +", "+ str(x[1] + d) +", "+ str(x[2]) +", 1.0};\n")
     f.write("// Define Lines in the boundary \n")
-    f.write("Line(1) = {1, 2};\n")
-    f.write("Line(2) = {2, 3};\n")
-    f.write("Line(3) = {3, 4};\n")
-    f.write("Line(4) = {4, 5};\n")
-    f.write("Line(5) = {5, 6};\n")
-    f.write("Line(6) = {6, 1};\n")
+    for i in range(0, 5):
+        f.write("Line("+ str(c + i) +") = {"+ str(c + i) +", "+ str(c + i + 1) +"};\n")
+    f.write("Line("+ str(c + 5) +") = {"+ str(c + 5) +", "+ str(c) +"};\n")
     f.write("// Boundary of the assembly \n")
-    f.write("Curve Loop(1) = {1, 2, 3, 4, 5, 6};\n")
+    f.write("Curve Loop("+ str(l) +") = {")
+    for i in range(1,6):
+        f.write(str(c + i) +", ")
+    f.write(str(c + 6) +"};\n")
     f.write("// Defines fuel channel Assembly\n")
-    c = 7
-    l = 2
-    ns = 1
+    c += 6
+    l += 1
+    ns += 1
     ls = [1]
-    
+        
     return c, l, ns, ls
 
 def place_circles(f, r, d_x, d_y, x, col, row, c, l, ns, ls):
@@ -100,32 +98,92 @@ def fuel_channels(f, d_x, rf, p_c, x, c, l, ns, ls):
 
     return c, l, ns, ls
 
-def place_fuel_assembly(f, d_x, rc, rf, p_c, x):
-    c, l, ns, ls = add_lines(f, d_x, x)
+def place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns):
+    c, l, ns, ls = place_hexagon(f, d_x, x, c, l, ns)
     c, l, ns, ls = cooling_channels(f, d_x, rc, p_c, x, c, l, ns, ls)
     c, l, ns, ls = fuel_channels(f, d_x, rf, p_c, x, c, l, ns, ls)
 
+    """
     f.write("Plane Surface("+str(ns)+") = {")
     for i in ls:
         if i == ls[-1]:
             f.write(str(i))
         else:
            f.write(str(i)+", ")
-    f.write("};\n")   
+    f.write("};\n")  
+    """ 
     return c, l, ns, ls
+
 
 def main():    
     f = open("untitled.geo","w+")
+    f.write("//+\n")
+    f.write("SetFactory('OpenCASCADE');\n")
 
     d_x = 30  # Side of hexagonal assembly
     rc = 0.5 # Radius of cooling channel
     rf = 1.5 # Radius of cooling channel
     p_c = 5.6 # pitch between channels 
 
-    x = [0, 0, 0]
-    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x)
-    # x = [60, 0, 0]
-    # c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x)
+    p = 2*d_x/2/np.tan(np.pi/6)
+
+    c = 1
+    l = 1
+    ns = 0
+
+    # Fuel assemblies
+    x = [0, 2*p, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [0, p, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [0, -p, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [0, -2*p, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+
+    x = [-3/2*d_x, p/2, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [-3/2*d_x, -p/2, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [3/2*d_x, p/2, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [3/2*d_x, -p/2, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+
+    x = [-3/2*d_x, p*5/2, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [-3/2*d_x, -p*5/2, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [3/2*d_x, p*5/2, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [3/2*d_x, -p*5/2, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+
+    x = [-3*d_x, p, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [-3*d_x, 2*p, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [3*d_x, p, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [3*d_x, 2*p, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)  
+    x = [-3*d_x, -p, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [-3*d_x, -2*p, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [3*d_x, -p, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [3*d_x, -2*p, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+
+    x = [-9/2*d_x, p/2, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [-9/2*d_x, -p/2, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [9/2*d_x, p/2, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
+    x = [9/2*d_x, -p/2, 0]
+    c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns)
 
     f.close()
 
