5478be1336a20ba8cdbfab30abf8e9089a697ea7
robfairh <ref3@illinois.edu>
2019-10-18 09:04:29 -0500

    control rod assembly geometry 2d

--- usnc/control_assembly_geometry.py
+++ usnc/control_assembly_geometry.py
@@ -27,46 +27,66 @@ def add_lines(f, d_x):
     f.write("//+\n")
     f.write("SetFactory('OpenCASCADE');\n")
     f.write("//+\n")
+    c = 7
+    return c
 
-def cooling_channels(f, d_x, rc, p_c):
+def cooling_channels(f, d_x, rc, p_c, c):
     s = 2 * p_c/2 * np.tan(np.pi/6)
     p = round(3*s, 4)
     p2 = round(3*s/2, 4)
 
-    c = 7
     col0 = [-3, 3]
     for j in col0:
         for i in range(0,3):
             f.write("Circle("+ str(i + c) +") = { "+ str((i-1)*p) +", "+ str(j*p_c) +", 0, "+ str(rc) +", 0, 2*Pi};\n")
         c += 3
     
-    for i in range(0,5):
-        f.write("Circle("+ str(i + c) +") = { "+ str((i-2)*p) +", "+ str(0) +", 0, "+ str(rc) +", 0, 2*Pi};\n")
-    c += 5
+    row0 = [-2, -1, 1, 2]
+    cc = 0
+    for i in row0:
+        f.write("Circle("+ str(cc + c) +") = { "+ str(i*p) +", "+ str(0) +", 0, "+ str(rc) +", 0, 2*Pi};\n")
+        cc += 1
+    c += cc
 
     col1 = [-3/2, 3/2]
     for j in col1:
         for i in range(0,4):
             f.write("Circle("+ str(i + c) +") = { "+ str((i*2-3)*p2) +", "+ str(j*p_c) +", 0, "+ str(rc) +", 0, 2*Pi};\n")
-        c += 4  
+        c += 4
 
-def fuel_channels(f, d_x, rf, p_c):
+    return c
+
+def fuel_channels(f, d_x, rf, p_c, c):
     s = 2 * p_c/2 * np.tan(np.pi/6)
     p = round(3*s, 4)
     p2 = round(3*s/2, 4)
 
-    c = 26
     col0 = [-1/2, 1/2]
-    for j in range(0,2):
-        for i in range(0,6):
-            f.write("Circle("+ str(i + c) +") = { "+ str((i*2-5)*p2) +", "+ str((2*j-1)/2*p_c) +", 0, "+ str(rf) +", 0, 2*Pi};\n")
-        c += 6
+    row0 = [-5, -3, 3, 5]
+    for j in col0:
+        cc = 0
+        for i in row0:
+            f.write("Circle("+ str(cc + c) +") = { "+ str(i*p2) +", "+ str(j*p_c) +", 0, "+ str(rf) +", 0, 2*Pi};\n")
+            cc += 1
+        c += cc
 
-    col1 = [-2, -1, 1, 2]
+    col1 = [-1, 1]
+    row1 = [-2, -1, 1, 2]
     for j in col1:
-        for i in range(0,5):
-            f.write("Circle("+ str(i + c) +") = { "+ str((i-2)*p) +", "+ str(j*p_c) +", 0, "+ str(rf) +", 0, 2*Pi};\n")
-        c += 5
+        cc = 0
+        for i in row1:
+            f.write("Circle("+ str(cc + c) +") = { "+ str(i*p) +", "+ str(j*p_c) +", 0, "+ str(rf) +", 0, 2*Pi};\n")
+            cc += 1
+        c += cc
+
+    col1 = [-2, 2]
+    row1 = [-2, -1, 0, 1, 2]
+    for j in col1:
+        cc = 0
+        for i in row1:
+            f.write("Circle("+ str(cc + c) +") = { "+ str(i*p) +", "+ str(j*p_c) +", 0, "+ str(rf) +", 0, 2*Pi};\n")
+            cc += 1
+        c += cc
 
     col2 = [-7/2, -5/2, 5/2, 7/2]
     for j in col2:
@@ -79,19 +99,27 @@ def fuel_channels(f, d_x, rf, p_c):
         for i in range(0,3):
             f.write("Circle("+ str(i + c) +") = { "+ str((i-1)*p) +", "+ str(j*p_c) +", 0, "+ str(rf) +", 0, 2*Pi};\n")
         c += 3
+    
+    return c
 
+def control_rod(f, rcb, c):
+    f.write("Circle("+ str(c) +") = { 0, 0, 0, "+ str(rcb) +", 0, 2*Pi};\n")
+    c += 1
+    return c
 
 def main():    
     f = open("untitled.geo","w+")
 
     d_x = 30  # Side of hexagonal assembly
-    rc = 0.5 # Radius of cooling channel
-    rf = 1.5 # Radius of cooling channel
+    rc = 0.5  # Radius of cooling channel
+    rf = 1.5  # Radius of cooling channel
     p_c = 5.6 # pitch between channels
+    rcb = 4   # Control bar radius
 
-    add_lines(f, d_x)
-    cooling_channels(f, d_x, rc, p_c)
-    fuel_channels(f, d_x, rf, p_c)
+    c = add_lines(f, d_x)
+    c = cooling_channels(f, d_x, rc, p_c, c)
+    c = fuel_channels(f, d_x, rf, p_c, c)
+    c = control_rod(f, rcb, c)
 
     f.close()
 
