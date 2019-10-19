60ab4131fb23ee5a00c283e04d4514b33e76417e
robfairh <ref3@illinois.edu>
2019-10-19 12:06:42 -0500

    all plane surfaces and boundaries

--- usnc/moltres_geometry2D.py
+++ usnc/moltres_geometry2D.py
@@ -1,62 +1,25 @@
 """
 
 """
-
 import numpy as np
 import math as mt
 import random as rd
 
-def place_hexagon(f, d_x, x, c, l, ns):
-    """
-    Adds boundary lines
-    returns:
-    c = number of lines
-    l =
-    ns =
-    """
-    d = d_x/2 * np.tan(np.pi/3)
-    d = round(d, 4)
-
-    f.write("// Define Points of the assembly boundaries \n")
-    f.write("Point("+ str(c) +") = { "+ str(x[0] + d_x/2) +", "+ str(x[1] + d) +", "+ str(x[2]) +", 1.0};\n")
-    f.write("Point("+ str(c + 1) +") = { "+ str(x[0] + d_x) +", "+ str(x[1]) +", "+ str(x[2]) +", 1.0};\n")
-    f.write("Point("+ str(c + 2) +") = { "+ str(x[0] + d_x/2) +", "+ str(x[1] - d) +", "+ str(x[2]) +", 1.0};\n")
-    f.write("Point("+ str(c + 3) +") = { "+ str(x[0] - d_x/2) +", "+ str(x[1] - d) +", "+ str(x[2]) +", 1.0};\n")
-    f.write("Point("+ str(c + 4) +") = { "+ str(x[0] - d_x) +", "+ str(x[1]) +", "+ str(x[2]) +", 1.0};\n")
-    f.write("Point("+ str(c + 5) +") = { "+ str(x[0] - d_x/2) +", "+ str(x[1] + d) +", "+ str(x[2]) +", 1.0};\n")
-    f.write("// Define Lines in the boundary \n")
-    for i in range(0, 5):
-        f.write("Line("+ str(c + i) +") = {"+ str(c + i) +", "+ str(c + i + 1) +"};\n")
-    f.write("Line("+ str(c + 5) +") = {"+ str(c + 5) +", "+ str(c) +"};\n")
-    f.write("// Boundary of the assembly \n")
-    # f.write("Curve Loop("+ str(l) +") = {")
-    # for i in range(0,6):
-    #     f.write(str(c + i) +", ")
-    # f.write(str(c + 5) +"};\n")
-    # f.write("// Defines fuel channel Assembly\n")
-    c += 6
-    l += 1
-    ns += 1
-    # ls = [1]
-        
-    return c, l, ns, ls
-
-def place_circles(f, r, d_x, d_y, x, col, row, c, l, ns, ls):
+def place_circles(f, r, d_x, d_y, x, col, row, c, l, ns):
     for j in col:
         cc = 0
         for i in row:
             f.write("Circle("+ str(cc + c) +") = { "+ str(x[0] + i*d_x) +", "+ str(x[1] + j*d_y) +", "+ str(x[2]) +", "+ str(r) +", 0, 2*Pi};\n")
             f.write("Curve Loop("+ str(l) +") = {"+ str(cc + c) +"};\n")
             f.write("Plane Surface("+ str(ns) +") = {"+ str(l) +"};\n")
-            ls.append(l)
             cc += 1
             l += 1
             ns += 1
         c += cc
     
-    return c, l, ns, ls
+    return c, l, ns
 
-def fuel_channels(f, d_x, rf, p_c, x, c, l, ns, ls, fuel=True):
+def fuel_channels(f, d_x, rf, p_c, x, c, l, ns, fuel=True):
     s = 2 * p_c/2 * np.tan(np.pi/6)
     p = round(3*s, 4)
     p2 = round(3*s/2, 4)
@@ -66,113 +29,77 @@ def fuel_channels(f, d_x, rf, p_c, x, c, l, ns, ls, fuel=True):
         row = [-5, -3, -1, 1, 3, 5]
     else:
         row = [-5, -3, 3, 5]
-    c, l, ns, ls = place_circles(f, rf, p2, p_c, x, col, row, c, l, ns, ls)
+    c, l, ns = place_circles(f, rf, p2, p_c, x, col, row, c, l, ns)
 
     col = [-7/2, -5/2, 5/2, 7/2]
     row = [-3, -1, 1, 3]
-    c, l, ns, ls = place_circles(f, rf, p2, p_c, x, col, row, c, l, ns, ls)
+    c, l, ns = place_circles(f, rf, p2, p_c, x, col, row, c, l, ns)
 
     col = [-2, 2]
     row = [-2, -1, 0, 1, 2]
-    c, l, ns, ls = place_circles(f, rf, p, p_c, x, col, row, c, l, ns, ls)
+    c, l, ns = place_circles(f, rf, p, p_c, x, col, row, c, l, ns)
 
     col = [-1, 1]
     if fuel == True:
         row = [-2, -1, 0, 1, 2]
     else:
         row = [-2, -1, 1, 2]
-    c, l, ns, ls = place_circles(f, rf, p, p_c, x, col, row, c, l, ns, ls)
+    c, l, ns = place_circles(f, rf, p, p_c, x, col, row, c, l, ns)
 
     col = [-4, 4]
     row = [-1, 0, 1]
-    c, l, ns, ls = place_circles(f, rf, p, p_c, x, col, row, c, l, ns, ls)
+    c, l, ns = place_circles(f, rf, p, p_c, x, col, row, c, l, ns)
 
-    return c, l, ns, ls
+    return c, l, ns
 
-def cooling_channels(f, d_x, rc, p_c, x, c, l, ns, ls, fuel=True):
+def cooling_channels(f, d_x, rc, p_c, x, c, l, ns, fuel=True):
     s = 2 * p_c/2 * np.tan(np.pi/6)
     p = round(3*s, 4)
     p2 = round(3*s/2, 4)
 
     col = [-3, 3]
     row = [-1, 0, 1]
-    c, l, ns, ls = place_circles(f, rc, p, p_c, x, col, row, c, l, ns, ls)
+    c, l, ns = place_circles(f, rc, p, p_c, x, col, row, c, l, ns)
 
     col = [-3/2, 3/2]
     row = [-3, -1, 1, 3]
-    c, l, ns, ls = place_circles(f, rc, p2, p_c, x, col, row, c, l, ns, ls)
+    c, l, ns = place_circles(f, rc, p2, p_c, x, col, row, c, l, ns)
 
     col = [0]
     if fuel:
         row = [-2, -1, 0, 1, 2]
     else:
         row = [-2, -1, 1, 2]
-    c, l, ns, ls = place_circles(f, rc, p, p_c, x, col, row, c, l, ns, ls)
+    c, l, ns = place_circles(f, rc, p, p_c, x, col, row, c, l, ns)
     
-    return c, l, ns, ls
+    return c, l, ns
 
-def control_rod(f, rcb, x, c, l, ns, ls):
+def control_rod(f, rcb, x, c, l, ns):
     col=[0]
     row=[0]
-    c, l, ns, ls = place_circles(f, rcb, 0, 0, x, col, row, c, l, ns, ls)
+    c, l, ns = place_circles(f, rcb, 0, 0, x, col, row, c, l, ns)
 
-    return c, l, ns, ls
+    return c, l, ns
 
 def place_fuel_assembly(f, d_x, rc, rf, p_c, x, c, l, ns):
-    ls = [1]
-    # c, l, ns, ls = place_hexagon(f, d_x, x, c, l, ns)
-    c, l, ns, ls = cooling_channels(f, d_x, rc, p_c, x, c, l, ns, ls, True)
-    c, l, ns, ls = fuel_channels(f, d_x, rf, p_c, x, c, l, ns, ls, True)
+    c, l, ns = cooling_channels(f, d_x, rc, p_c, x, c, l, ns, True)
+    c, l, ns = fuel_channels(f, d_x, rf, p_c, x, c, l, ns, True)
 
-    """
-    f.write("Plane Surface("+str(ns)+") = {")
-    for i in ls:
-        if i == ls[-1]:
-            f.write(str(i))
-        else:
-           f.write(str(i)+", ")
-    f.write("};\n")  
-    """ 
-    return c, l, ns, ls
+    return c, l, ns
 
 def place_control_assembly(f, d_x, rc, rf, rcb, p_c, x, c, l, ns):
-    ls = [1]
-    # c, l, ns, ls = place_hexagon(f, d_x, x, c, l, ns)
-    c, l, ns, ls = cooling_channels(f, d_x, rc, p_c, x, c, l, ns, ls, False)
-    c, l, ns, ls = fuel_channels(f, d_x, rf, p_c, x, c, l, ns, ls, False)
-    c, l, ns, ls = control_rod(f, rcb, x, c, l, ns, ls)
+    c, l, ns = cooling_channels(f, d_x, rc, p_c, x, c, l, ns, False)
+    c, l, ns = fuel_channels(f, d_x, rf, p_c, x, c, l, ns, False)
+    c, l, ns = control_rod(f, rcb, x, c, l, ns)
 
-    """
-    f.write("Plane Surface("+str(ns)+") = {")
-    for i in ls:
-        if i == ls[-1]:
-            f.write(str(i))
-        else:
-           f.write(str(i)+", ")
-    f.write("};\n")  
-    """ 
-    return c, l, ns, ls
+    return c, l, ns
 
 def place_central_assembly(f, d_x, rcc, x, c, l, ns):
-    ls = [1]
-    # c, l, ns, ls = place_hexagon(f, d_x, x, c, l, ns)
-    c, l, ns, ls = control_rod(f, rcc, x, c, l, ns, ls)
+    c, l, ns = control_rod(f, rcc, x, c, l, ns)
 
-    """
-    f.write("Plane Surface("+str(ns)+") = {")
-    for i in ls:
-        if i == ls[-1]:
-            f.write(str(i))
-        else:
-           f.write(str(i)+", ")
-    f.write("};\n")  
-    """ 
-    return c, l, ns, ls
+    return c, l, ns
 
 def place_reflector(f, d_x, rr1, rr2, x, c, l, ns):
-    ls = [1]
-    # c, l, ns, ls = place_hexagon(f, d_x, x, c, l, ns)
-
     f.write("// Reflector \n")
     f.write("Circle("+ str(c) +") = { "+ str(x[0]) +", "+ str(x[1]) +", "+ str(x[2]) +", "+ str(rr1) +", 0, 2*Pi};\n")
     f.write("Curve Loop("+ str(l) +") = {"+ str(c) +"};\n")
@@ -195,32 +122,6 @@ def place_reflector(f, d_x, rr1, rr2, x, c, l, ns):
     l += 2
     ns += 2
     return c, l, ns
-
-def place_circles(f, r, d_x, d_y, x, col, row, c, l, ns, ls):
-    for j in col:
-        cc = 0
-        for i in row:
-            f.write("Circle("+ str(cc + c) +") = { "+ str(x[0] + i*d_x) +", "+ str(x[1] + j*d_y) +", "+ str(x[2]) +", "+ str(r) +", 0, 2*Pi};\n")
-            f.write("Curve Loop("+ str(l) +") = {"+ str(cc + c) +"};\n")
-            f.write("Plane Surface("+ str(ns) +") = {"+ str(l) +"};\n")
-            ls.append(l)
-            cc += 1
-            l += 1
-            ns += 1
-        c += cc
-    
-    return c, l, ns, ls
-
-    """
-    f.write("Plane Surface("+str(ns)+") = {")
-    for i in ls:
-        if i == ls[-1]:
-            f.write(str(i))
-        else:
-           f.write(str(i)+", ")
-    f.write("};\n")  
-    """ 
-    return c, l, ns, ls
 
 def main():    
     f = open("untitled.geo","w+")
@@ -264,12 +165,12 @@ def main():
         x[i,:] = [0, (19-i)*p, 0]
    
     for i in assemblies['control']:
-        c, l, ns, ls = place_control_assembly(f, d_x, rc, rf, rcb, p_c, x[i], c, l, ns)
+        c, l, ns = place_control_assembly(f, d_x, rc, rf, rcb, p_c, x[i], c, l, ns)
 
     for i in assemblies['fuel']:
-        c, l, ns, ls = place_fuel_assembly(f, d_x, rc, rf, p_c, x[i], c, l, ns)
+        c, l, ns = place_fuel_assembly(f, d_x, rc, rf, p_c, x[i], c, l, ns)
     
-    c, l, ns, ls = place_central_assembly(f, d_x, rcc, x[19], c, l, ns)
+    c, l, ns = place_central_assembly(f, d_x, rcc, x[19], c, l, ns)
     c, l, ns = place_reflector(f, d_x, rr1, rr2, x[19], c, l, ns)
 
     f.close()
