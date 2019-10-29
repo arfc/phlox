7b4fd26ae2744850db63c5607ec74b1aaa579d17
robfairh <ref3@illinois.edu>
2019-10-29 12:03:16 -0500

    1 arc or 1 circle and lower line

--- usnc/msh_files/get_arc.py
+++ usnc/msh_files/get_arc.py
@@ -26,7 +26,7 @@ def check_intersection(x, y, r, a):
         intersect = False
     return intersect
 
-def plot_line_circle(r, R, a, x, y):
+def plot_line_circle(r, R, a, x, y, l, ps):
     """
     plots circle and line
     r: radius of the circle
@@ -38,17 +38,19 @@ def plot_line_circle(r, R, a, x, y):
     f = open("untitled.geo","w+")
     f.write('// Gmsh whatever\n')
     f.write('SetFactory("OpenCASCADE");\n//+\n')
-    f.write("Point(1) = { 0, 0, 0, 1.0};\n")
+    f.write("Circle("+ str(l+1) +") = { "+ str(x) +", "+ str(y) +", 0, "+ str(r) +", 0, 2*Pi};\n")
+    f.write("Point("+ str(ps+2) +") = { 0, 0, 0, 1.0};\n")
     X = R * np.cos(a)
     Y = R * np.sin(a)
-    f.write("Point(2) = { "+ str(X) +", "+ str(Y) +", 0, 1.0};\n")
-    f.write("Point(3) = { "+ str(x) +", "+ str(y) +", 0, 1.0};\n")
-    f.write("Line(1) = { 1, 2};\n")
-    f.write("Circle(2) = { "+ str(x) +", "+ str(y) +", 0, "+ str(r) +", 0, 2*Pi};\n")
-    # f.write("Circle("+ str(c) +") = { "+ str(x[0] + i*d_x) +", "+ str(x[1] + j*d_y) +", "+ str(x[2]) +", "+ str(r) +", "+ str(a1) +", "+ str(a2) +"};\n")
+    f.write("Point("+ str(ps+3) +") = { "+ str(X) +", "+ str(Y) +", 0, 1.0};\n")
+    f.write("Line("+ str(l+2) +") = { "+ str(ps+2) +", "+ str(ps+3) +"};\n")
     f.close()
 
-def plot_arc(r, R, a, x, y):
+    l += 2
+    ps += 3
+    return l, ps
+
+def plot_arc(r, R, a, x, y, l, ps):
     """
     plots arc based on the intersection of the line and the circle
     r: radius of the circle
@@ -74,33 +76,35 @@ def plot_arc(r, R, a, x, y):
     f = open("untitled.geo","w+")
     f.write('// Gmsh whatever\n')
     f.write('SetFactory("OpenCASCADE");\n//+\n')
-    f.write("Circle(1) = { "+ str(x) +", "+ str(y) +", 0, "+ str(r) +", "+ str(alpha1) +", "+ str(alpha2) +"};\n")
-    # f.write("Circle("+ str(c) +") = { "+ str(x[0] + i*d_x) +", "+ str(x[1] + j*d_y) +", "+ str(x[2]) +", "+ str(r) +", "+ str(a1) +", "+ str(a2) +"};\n")
-
-    f.write("Point(3) = { 0, 0, 0, 1.0};\n")
+    f.write("Circle("+ str(l+1) +") = { "+ str(x) +", "+ str(y) +", 0, "+ str(r) +", "+ str(alpha1) +", "+ str(alpha2) +"};\n")
+    f.write("Point("+ str(ps+3) +") = { 0, 0, 0, 1.0};\n")
     X = R * np.cos(a)
     Y = R * np.sin(a)
-    f.write("Point(4) = { "+ str(X) +", "+ str(Y) +", 0, 1.0};\n")
-    f.write("Point(5) = { "+ str(x) +", "+ str(y) +", 0, 1.0};\n")
-    f.write("Line(2) = { 3, 1};\n")
-    f.write("Line(3) = { 1, 2};\n")
-    f.write("Line(4) = { 2, 4};\n")
+    f.write("Point("+ str(ps+4) +") = { "+ str(X) +", "+ str(Y) +", 0, 1.0};\n")
+    f.write("Line("+ str(l+2) +") = { "+ str(ps+3) +", "+ str(ps+1) +"};\n")
+    f.write("Line("+ str(l+3) +") = { "+ str(ps+1) +", "+ str(ps+2) +"};\n")
+    f.write("Line("+ str(l+4) +") = { "+ str(ps+2) +", "+ str(ps+4) +"};\n")
 
     f.close()
+    l += 4
+    ps += 4
+    return l, ps
 
 def main():    
     r = 1        # Channel radius
     R = 5        # Moderator Radius
     a1 = np.pi/6 # angle of the plane
     # x defines the center of the circle
-    # x = [ 2, 0.2]  # intersection above the center
-    # x = [ 2, 1]  # intersection below the center
-    x = [ 2, 3]  # no intersection
+    x = [ 2, 0.2]  # intersection above the center
+    #x = [ 2, 1]  # intersection below the center
+    #x = [ 2, 3]  # no intersection
     
+    l = 0
+    ps = 0
     if not check_intersection(x[0], x[1], r, a1):
-        plot_line_circle(r, R, a1, x[0], x[1])
+        l, ps = plot_line_circle(r, R, a1, x[0], x[1], l, ps)
     else:
-        plot_arc(r, R, a1, x[0], x[1])
+        l, ps = plot_arc(r, R, a1, x[0], x[1], l, ps)
 
 if __name__ == "__main__":
     main()
