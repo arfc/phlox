c625c7d7427c5c225560be7d6ff14dea05b23a3d
robfairh <ref3@illinois.edu>
2019-10-29 12:52:29 -0500

    1 arc, multiple circles, lines

--- usnc/msh_files/get_arc.py
+++ usnc/msh_files/get_arc.py
@@ -16,7 +16,7 @@ def check_intersection(x, y, r, a):
     xo = x + dx
     yo = y - dy
     ac = mt.atan(yo/xo)
-    print("ac: ", ac/np.pi*180)
+    # print("ac: ", ac/np.pi*180)
 
     if ac < a:
         print("Intersection")
@@ -26,31 +26,24 @@ def check_intersection(x, y, r, a):
         intersect = False
     return intersect
 
-def plot_line_circle(r, R, a, x, y, l, ps):
+def plot_circle(f, r, R, x, y, l, ps, dict_type, lp):
     """
-    plots circle and line
+    plots circles
     r: radius of the circle
     R: radius of the moderator
     a: angle of the reactor cut
     x,y: center of the circle
     """
     
-    f = open("untitled.geo","w+")
-    f.write('// Gmsh whatever\n')
-    f.write('SetFactory("OpenCASCADE");\n//+\n')
     f.write("Circle("+ str(l+1) +") = { "+ str(x) +", "+ str(y) +", 0, "+ str(r) +", 0, 2*Pi};\n")
-    f.write("Point("+ str(ps+2) +") = { 0, 0, 0, 1.0};\n")
-    X = R * np.cos(a)
-    Y = R * np.sin(a)
-    f.write("Point("+ str(ps+3) +") = { "+ str(X) +", "+ str(Y) +", 0, 1.0};\n")
-    f.write("Line("+ str(l+2) +") = { "+ str(ps+2) +", "+ str(ps+3) +"};\n")
-    f.close()
+    dict_type['circle'].append(l+1)
+    lp.append(ps+1)
 
-    l += 2
-    ps += 3
-    return l, ps
+    l += 1
+    ps += 1
+    return l, ps, dict_type, lp
 
-def plot_arc(r, R, a, x, y, l, ps):
+def plot_arc(f, r, R, a, x, y, l, ps, dict_type, lp):
     """
     plots arc based on the intersection of the line and the circle
     r: radius of the circle
@@ -59,52 +52,71 @@ def plot_arc(r, R, a, x, y, l, ps):
     x,y: center of the circle
     """
 
-    alpha1 = 0
-    # alpha2 = 3/2*np.pi
-
     y1 = x * np.tan(a)
     dy = y - y1
     d = dy * np.cos(a) # center-line distance
     alpha = mt.acos(d/r)
     alpha2 = 3./2*np.pi - (alpha - a)   
     alpha1 = -1./2*np.pi + (alpha + a)
-    # print("a: ", a/np.pi*180)
-    # print("alpha: ", alpha/np.pi*180)   
-    print("alpha1: ", alpha1/np.pi*180)
-    print("alpha2: ", alpha2/np.pi*180)
+    #print("alpha1: ", alpha1/np.pi*180)
+    #print("alpha2: ", alpha2/np.pi*180)
 
-    f = open("untitled.geo","w+")
-    f.write('// Gmsh whatever\n')
-    f.write('SetFactory("OpenCASCADE");\n//+\n')
     f.write("Circle("+ str(l+1) +") = { "+ str(x) +", "+ str(y) +", 0, "+ str(r) +", "+ str(alpha1) +", "+ str(alpha2) +"};\n")
-    f.write("Point("+ str(ps+3) +") = { 0, 0, 0, 1.0};\n")
+    dict_type['arc'].append(l+1)
+    points = []
+    points.append(ps + 1)
+    points.append(ps + 2)
+    lp.append(points)
+    
+    l += 1
+    ps += 2
+    return l, ps, dict_type, lp
+
+def plot_lines(f, R, a, l, ps, dict_type, lp):
     X = R * np.cos(a)
     Y = R * np.sin(a)
-    f.write("Point("+ str(ps+4) +") = { "+ str(X) +", "+ str(Y) +", 0, 1.0};\n")
-    f.write("Line("+ str(l+2) +") = { "+ str(ps+3) +", "+ str(ps+1) +"};\n")
-    f.write("Line("+ str(l+3) +") = { "+ str(ps+1) +", "+ str(ps+2) +"};\n")
-    f.write("Line("+ str(l+4) +") = { "+ str(ps+2) +", "+ str(ps+4) +"};\n")
-
-    f.close()
-    l += 4
-    ps += 4
-    return l, ps
+    f.write("Point("+ str(ps+1) +") = { 0, 0, 0, 1.0};\n")  
+    f.write("Point("+ str(ps+2) +") = { "+ str(X) +", "+ str(Y) +", 0, 1.0};\n")
+    if ps-l == 0:
+        f.write("Line("+ str(l+1) +") = { "+ str(ps+1) +", "+ str(ps+2) +"};\n")
+    else:
+        f.write("Line("+ str(l+1) +") = { "+ str(ps+1) +", "+ str(lp[dict_type['arc'][0]][0]) +"};\n")
+        f.write("Line("+ str(l+2) +") = { "+ str(lp[dict_type['arc'][0]][0]) +", "+ str(lp[dict_type['arc'][0]][1]) +"};\n")
+        f.write("Line("+ str(l+3) +") = { "+ str(lp[dict_type['arc'][0]][1]) +", "+ str(ps+2) +"};\n")
 
 def main():    
     r = 1        # Channel radius
-    R = 5        # Moderator Radius
+    R = 7        # Moderator Radius
     a1 = np.pi/6 # angle of the plane
     # x defines the center of the circle
-    x = [ 2, 0.2]  # intersection above the center
-    #x = [ 2, 1]  # intersection below the center
-    #x = [ 2, 3]  # no intersection
+    x = np.zeros((2,2))
+    # x[0] = [ 2, 0.2]  # intersection above the center
+    x[0] = [ 2, 1]  # intersection below the center
+    x[1] = [ 2, 4]  # no intersection
+    #x[2] = [ 4, 3]  # no intersection
     
     l = 0
     ps = 0
-    if not check_intersection(x[0], x[1], r, a1):
-        l, ps = plot_line_circle(r, R, a1, x[0], x[1], l, ps)
-    else:
-        l, ps = plot_arc(r, R, a1, x[0], x[1], l, ps)
+
+    dict_type = {'circle':[], 'arc':[]}
+    lp = [0] # list of points
+    
+    f = open("untitled.geo","w+")
+    f.write('// Gmsh whatever\n')
+    f.write('SetFactory("OpenCASCADE");\n//+\n')
+
+    for i in range(len(x)):
+        if not check_intersection(x[i,0], x[i,1], r, a1):
+            l, ps, dict_type, lp = plot_circle(f, r, R, x[i,0], x[i,1], l, ps, dict_type, lp)
+        else:
+            l, ps, dict_type, lp = plot_arc(f, r, R, a1, x[i,0], x[i,1], l, ps, dict_type, lp)
+    
+    print(dict_type)
+    print(lp)
+    print(lp[dict_type['arc'][0]])
+    plot_lines(f, R, a1, l, ps, dict_type, lp)
+    
+    f.close()        
 
 if __name__ == "__main__":
     main()
